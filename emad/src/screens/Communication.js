37542712ac7877f09952c52c286f522d9ab8992c
AntonioPappalardo <a.pappalardo36@studenti.unisa.it>
2021-11-15 15:03:09 +0100

    update of searchuser and communication pages

--- emad/src/screens/Communication.js
+++ emad/src/screens/Communication.js
@@ -2,39 +2,49 @@ import React from "react";
 import { StyleSheet, TextInput, View, Text, TouchableOpacity, ScrollView } from "react-native";
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import Ionicons from 'react-native-vector-icons/Ionicons'
-import { LinearGradient } from 'expo-linear-gradient';
-import Container from "../components/Container";
+import InputButton from "../components/InputButton";
 
 
 
 const Communication = ({navigation}) => {
-    const [Message,onChangeText]= React.useState('Message');
+    const [Message,onChangeText]= React.useState('');
     return (
       <View style={{backgroundColor:"#2A2E43",height:"100%"}}>
-        <View style={{top:35,flexDirection:"row",justifyContent:"flex-start"}} onPress= {
-            () => navigation.navigate('User') }>
-          <Ionicons name="chevron-back" size={30} color="white"/>
-          <Text style={{ fontSize:25,color:'white',fontWeight:'bold'}}>
+        <View style={{top:35,flexDirection:"row",justifyContent:"flex-start"}} >
+          <Ionicons name="chevron-back" size={30} color="white" onPress= {
+            () => { onChangeText("");navigation.goBack()}}/>
+          <Text style={{ fontSize:25,color:'white',fontWeight:'bold'}} onPress= {
+            () => { onChangeText("");navigation.goBack()}}>
               Torna Indietro
           </Text>
         </View>
         <View
           style={{marginTop:75,backgroundColor:"#363A4E",height:375,marginLeft:20,marginRight:20,borderRadius:25}}
         >
-          <TextInput
-            editable
-            maxLength={250}
-            multiline
-            color="white"
-            numberOfLines={10}
-            onChangeText={text => onChangeText(text)}
-            value={Message}
-          />
+          <View style={{flexDirection:"row",marginLeft:25,marginTop:50,alignContent:"flex-start"}}>
+            <Ionicons name="md-mail-open-outline" color="white" size={30}/>
+            <TextInput
+              editable
+              maxLength={250}
+              multiline
+              color="white"
+              fontSize={25}
+              numberOfLines={10}
+              placeholder="Messaggio"
+              placeholderTextColor="white"
+              onChangeText={text => onChangeText(text)}
+              value={Message}
+              style={{textAlignVertical:"top",marginLeft:10,width:250}}
+            />
+          </View>
+
         </View>
-        <View style={{marginTop:25,flexDirection:"row"}} alignSelf="center">
-          <MaterialCommunityIcons name="telegram" color="#0078B4"size={30}/>
-          <MaterialCommunityIcons name="whatsapp" color="green"size={30}/>
+        <View style={{marginTop:25,flexDirection:"row",width:100,justifyContent:"center"}} alignSelf="center">
+          <MaterialCommunityIcons name="telegram" color="#0078B4"size={35}  onPress={() => {console.log("Apri Telegram "+Message)}}/>
+          <MaterialCommunityIcons name="whatsapp" color="green"size={35}  onPress={() => {console.log("Apri Whatsapp "+Message)}}/>
         </View>
+        <InputButton params={{ marginTop: 100, width: "75%" }} name="Invia" icon="arrow-forward-outline" rotation="-45deg" onPress={() => {console.log("Invia Messaggio "+Message)}} />
+
 
       </View>
     )
