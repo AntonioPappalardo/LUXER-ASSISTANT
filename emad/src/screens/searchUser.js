051046f4a8d7290cc118d212bb84bc3b872c5c33
AntonioPappalardo <a.pappalardo36@studenti.unisa.it>
2021-11-29 13:00:33 +0100

    Revert "Merge branch 'main' of https://github.com/FrancescoPa96/Emad2021_Retail-Fashion-Luxury"
    This reverts commit f05cb5d59089a6cc97458cee1ad1b3cf9c633645, reversing
    changes made to b5aa670c3d701e992071720a76969e20479c37b8.

--- emad/src/screens/searchUser.js
+++ emad/src/screens/searchUser.js
@@ -1,77 +1,89 @@
 import React from "react";
-import {Image, View, Text, TouchableOpacity, ScrollView, TouchableHighlight } from "react-native";
-import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
+import { StyleSheet, Image, View, Text, TouchableOpacity, ScrollView } from "react-native";
 import Ionicons from 'react-native-vector-icons/Ionicons'
-import BackButton from "../components/BackButton";
-import Divider from "../components/Divider";
+import Box from "../components/Box";
 import InputText from "../components/InputText";
-import { useTheme } from "../theme/ThemeProvider";
-import AppLoading from 'expo-app-loading';
-import { useFonts } from 'expo-font';
+import { findFocusedRoute } from "@react-navigation/core";
+import { Appearance } from 'react-native';
+import dark from '../../src/theme/dark';
+import light from '../../src/theme/light';
+import { shadow } from "react-native-paper";
+const colorScheme = Appearance.getColorScheme();
+
+if (colorScheme === 'dark') {
+    var colorTheme = dark;
+} else {
+    var colorTheme = light;
+}
 
 const users = [
-    { "name": "Maria Rossi", "id": "001", "next_appointment": "", "reserved": "true" },
-    { "name": "Antonella Rossi", "id": "002", "next_appointment": "", "reserved": "true" },
-    { "name": "Margherita Rosi", "id": "003", "next_appointment": "", "reserved": "true" },
-    { "name": "Maria Bianchi", "id": "004", "next_appointment": "", "reserved": "true" },
-    { "name": "Michela Gargiulo", "id": "005", "next_appointment": "29 Novembre 2021 15:00-16:00", "reserved": "true" },
+    { "name": "Maria Rossi", "id": "001" },
+    { "name": "Antonella Rossi", "id": "002" },
+    { "name": "Margherita Rosi", "id": "003" },
+    { "name": "Maria Bianchi", "id": "004" },
+    { "name": "Michela Gargiulo", "id": "005" },
 ]
-
 const searchUser = ({ navigation }) => {
-    
-    const {colors, isDark} = useTheme();
-
-    const tabBarHeight = useBottomTabBarHeight();
     const [search, onChangeText] = React.useState('');
     const [user, onSearch] = React.useState([]);
     const ook = (cerca) => {
+        console.log(users)
         onChangeText(cerca);
         onSearch(users.filter(user => (user.name.toLowerCase().includes(cerca.toLowerCase()) || user.id.includes(cerca))))
 
     }
-    let [fontsLoaded] = useFonts({
-        'SFProDisplayMedium': require('../../assets/fonts/SFProDisplayMedium.otf'),
-        'SFProDisplayRegular': require('../../assets/fonts/SFProDisplayRegular.otf'),
-    });
-
-    if (!fontsLoaded) {
-        return <AppLoading />;
-    } else {
-        return (
-            
-            <View style={{ backgroundColor: colors.theme.background, flex: 1 }}>
-                <BackButton onPress={() => { navigation.goBack() }} />
-                <View style={{ alignItems: "center", marginBottom: 15 }}>
-                    <InputText params={{ width: "75%", paddingLeft: 75, textAlign: "left" }}
-                        name="Cliente" icon="search" rotation="0deg" value={search} onChangeText={cerca => ook(cerca)} secure='false' />
-                </View>
-                <Divider width="100%" />
-                <ScrollView>
-                    {user.map((item) => (
-                        <View key={item.id} style={{height: 75, width: "90%",flexDirection: "row", alignSelf: "center",marginTop: 5, marginBottom: 5, }}  >
-                                <View style={{width: '25%'}}>
-                                <View style={{ justifyContent: "center", marginLeft: 5, height: 70, width: 70, shadowOffset: { width: 1, height: 2 },shadowOpacity: 0.25,shadowRadius: 5, marginRight: 10, borderRadius: 5 }}>
-                                    <Image source={require('../../assets/img/img.jpg')} style={{ height: 70, width: 70, borderRadius: 5, borderWidth: 3, borderColor: "white" }} />
-                                </View>
-                                </View>
-                                <TouchableOpacity style={{flexDirection: 'row', width: '75%'}} activeOpacity={.75} onPress={() => { navigation.navigate('UserPage',{user:item.id}) }}>
-                                    <View style={{ flexDirection: "column", justifyContent: "center" }}>
-                                        <Text style={{ fontSize: 16, fontFamily: 'SFProDisplayMedium', color: colors.theme.title }}>{item.name}</Text>
-                                        <Text style={{ fontSize: 11, fontFamily: 'SFProDisplayRegular', color: colors.theme.subtitle }}>Codice cliente: {item.id}</Text>
-                                        <Text style={{ fontSize: 12, fontFamily: 'SFProDisplayRegular', color: colors.theme.title }}>{item.next_appointment}</Text>
-                                    </View>
-                                    <View style={{ justifyContent: 'center', alignContent: "center", alignItems: 'center', marginLeft: 'auto', top: 15, marginRight: 5, height: 40, width: 40}}>
-                                        <Ionicons name="chevron-forward" size={25} color={colors.theme.title} />
-                                    </View>
-                                </TouchableOpacity>
-                        </View>
-                    ))}
-                    <View style={{ marginBottom: tabBarHeight + 10 }}></View>
-
-                </ScrollView>
+    return (
+        <View style={{ backgroundColor: colorTheme.theme.background, height: "100%" }}>
+            <View style={{ top: 35, flexDirection: "row", justifyContent: "flex-start" }} >
+                <Ionicons name="chevron-back" size={25} color="white" onPress={
+                    () => { onChangeText(""); navigation.goBack() }} />
+                <Text style={{ fontSize: 20, color:colorTheme.theme.background, fontWeight: 'bold' }} onPress={
+                    () => { onChangeText(""); navigation.goBack() }}>
+                    Torna Indietro
+                </Text>
             </View>
-        )
-    }
+            <View>
+                <Text style={styles.text}>Ricerca Cliente</Text>
+                <InputText params={{ marginTop: 70, width: "100%", paddingLeft: 75, textAlign: "left" }}
+                    name="Cliente" icon="search" rotation="0deg" value={search} onChangeText={cerca => ook(cerca)} secure='false' />
+                <ScrollView style={{ marginTop: 50 }}>
+                    {user.map((item) => (
+                        <TouchableOpacity key={item.id}>
+                            <View style={{ height: 75, width: "90%", justifyContent: "space-between", flexDirection: "row", marginBottom: 10, alignSelf: "center", borderRadius: 5 }}>
+                                <View style={{ justifyContent: "center", marginLeft: 5, height:70,width: 70 ,borderRadius: 10, border:0.8,borderColor:"black", shadowOffset: { width: 10, height: 10 }, shadowColor: '#000',shadowOpacity: 1,elevation: 10,marginRight:10}}>
+                                    <Image source={require('../../assets/img/img.jpg')} style={{ height: 65, width: 65,borderRadius: 10 }}/>
+                                </View>
+                                <View style={{ height: 75, width: 150, flexDirection: "column", justifyContent: "center" }}>
+                                    <Text style={{ fontSize: 20, color:colorTheme.theme.title}}>{item.name}</Text>
+                                    <Text style={{ fontSize: 10, color: colorTheme.theme.subtitle }}>Codice cliente: {item.id}</Text>
+                                </View>
+                                <View style={{ justifyContent: 'center', alignContent: "center", alignItems: 'center', marginLeft: 'auto', top: 15, marginRight: 5, height: 40, width: 40}}>
+                                    <Ionicons name="chevron-forward" size={25} color={colorTheme.theme.title} />
+                                </View>
+                            </View>
+                        </TouchableOpacity>
+                    )
+                    )}
+                </ScrollView>
+
+            </View>
+
+        </View>
+    )
 };
+
+const styles = StyleSheet.create({
+    text: {
+        alignSelf: 'center',
+        top: 50,
+        fontSize: 25,
+        color: colorTheme.theme.title,
+        fontWeight: 'bold'
+    },
+    screen: {
+        height: "100%",
+        backgroundColor: "#2A2E43"
+    }
+});
 
 export default searchUser;
