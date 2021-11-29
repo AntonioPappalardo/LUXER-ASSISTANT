0af8c9a1408da07d7a6556e003db67abdf37b7c3
Mattia Limone <m.limone7@studenti.unisa.it>
2021-11-29 13:42:41 +0100

    fix Margin

--- emad/src/components/SwitchItem.js
+++ emad/src/components/SwitchItem.js
@@ -21,24 +21,20 @@ const SwitchItem = (props) => {
         return <AppLoading />;
     } else {
         return (
-            <View style={{marginTop:10}}>
-            <TouchableOpacity activeOpacity={1} onPress={props.onPress} style={{ width: "75%", height: 48, alignSelf: "center" }}>
-                <View style={{ flexDirection: 'row', justifyContent: 'space-between'}}>
-                    <Text style={{ color: colors.theme.primary, fontFamily: "SFProDisplayMedium", paddingBottom: 13 }}>{props.title}</Text>
-                    <Switch style={styles.switch} trackColor={{ false: "#767577", true: "#66d871" }} 
+            <View style={{width: "75%",height: 48, alignSelf: "center",justifyContent:'center' }}>
+            <View style={{ flexDirection: 'row',height:'100%',justifyContent: 'space-between',alignItems:'center' }}>
+                    <Text style={{ color: colors.theme.primary, fontFamily: "SFProDisplayMedium" }}>{props.title}</Text>
+                    <Switch trackColor={{ false: "#767577", true: "#66d871" }} 
                     thumbColor={props.value ? "#f4f3f4" : "#f4f3f4"} ios_backgroundColor="#3e3e3e" onValueChange={props.onValueChange} value={props.value}/>
                 </View>
                 <Divider width={"100%"} />
-                </TouchableOpacity>
-                </View>
+            </View>
         )
     }
 };
 
 const styles = StyleSheet.create({
-    switch: {
-        marginTop:-8
-    }
+    
   });
 
 export default SwitchItem;
