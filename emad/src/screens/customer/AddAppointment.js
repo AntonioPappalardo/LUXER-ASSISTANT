6f554e77a0969c958720759a4cd342eb2ffd0aac
Mattia Limone <m.limone7@studenti.unisa.it>
2021-12-14 10:57:02 +0100

    add CheckBox and Select

--- emad/src/screens/customer/AddAppointment.js
+++ emad/src/screens/customer/AddAppointment.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { StyleSheet, Image, View, Text, TouchableOpacity, ScrollView, Dimensions } from "react-native";
-import { Checkbox } from 'react-native-paper';
-import Ionicons from 'react-native-vector-icons/Ionicons'
+import { StyleSheet, View, Text, ScrollView, Dimensions } from "react-native";
+import Checkbox from 'expo-checkbox';
+import { Picker } from '@react-native-picker/picker';
 import Divider from "../../components/Divider";
 import BackButton from "../../components/BackButton";
 import InputButton from "../../components/InputButton";
@@ -26,23 +26,27 @@ const AddAppointment = ({ navigation }) => {
 
     const { key, colors, setScheme, isDark } = useTheme();
 
+    const [isChecked, setChecked] = useState(false);
+    const [selectedFirstSlot, setSelectedFirstSlot] = useState();
+    const [selectedSecondSlot, setSelectedSecondSlot] = useState();
+
     const [daySelected, setDaySelected] = useState(new Date);
-    const [isSelected, setSelection] = useState(false);
+
 
     const onDayPress = day => {
         setDaySelected(day.dateString);
     }
     const onMonthChange = day => {
         var today = new Date;
-        today.setHours(0,0,0,0)
+        today.setHours(0, 0, 0, 0)
         var selected = new Date(day.dateString)
-        if(today.getTime() != selected.getTime()) {
+        if (today.getTime() != selected.getTime()) {
             setDaySelected(day.dateString);
         } else {
             setDaySelected(undefined)
         }
     }
-     
+
     const toggleScheme = () => {
         isDark ? setScheme('light') : setScheme('dark');
     }
@@ -51,29 +55,29 @@ const AddAppointment = ({ navigation }) => {
         if (date) {
             var Data = new Date(date)
             return (
-                <View style={{marginTop:'15%', zIndex:1}}>
-                <View style= {{position: 'absolute', top: "15%", alignSelf: 'center',zIndex: 1}}>
-                    <Text style={{ textTransform: 'capitalize', fontSize: 16, textAlign: 'left', color: colors.theme.primary}}>
-                        {moment(Data).format('dddd')}
-                    </Text>
-                    <Text style={{ textTransform: 'capitalize', fontSize: 28, textAlign: 'left', color: colors.theme.primary, marginBottom: 15 }}>
-                        {moment(Data).format('DD MMMM YYYY')}
-                    </Text>
-                </View>
+                <View style={{ marginTop: '15%', zIndex: 1 }}>
+                    <View style={{ position: 'absolute', top: "15%", alignSelf: 'center', zIndex: 1 }}>
+                        <Text style={{ textTransform: 'capitalize', fontSize: 16, textAlign: 'left', color: colors.theme.primary }}>
+                            {moment(Data).format('dddd')}
+                        </Text>
+                        <Text style={{ textTransform: 'capitalize', fontSize: 28, textAlign: 'left', color: colors.theme.primary, marginBottom: 15 }}>
+                            {moment(Data).format('DD MMMM YYYY')}
+                        </Text>
+                    </View>
                 </View>
             )
         }
         var Data = new Date()
-            return (
-            <View style= {{position: 'absolute', top: 0, alignSelf: 'center',zIndex: 1}}>
-                    <Text style={{ textTransform: 'capitalize', fontSize: 16, textAlign: 'left', color: colors.theme.primary}}>
-                        {moment(Data).format('dddd')}
-                    </Text>
-                    <Text style={{ textTransform: 'capitalize', fontSize: 28, textAlign: 'left', color: colors.theme.primary, marginBottom: 15 }}>
-                        {moment(Data).format('DD MMMM YYYY')}
-                    </Text>
-                </View>
-            )
+        return (
+            <View style={{ position: 'absolute', top: 0, alignSelf: 'center', zIndex: 1 }}>
+                <Text style={{ textTransform: 'capitalize', fontSize: 16, textAlign: 'left', color: colors.theme.primary }}>
+                    {moment(Data).format('dddd')}
+                </Text>
+                <Text style={{ textTransform: 'capitalize', fontSize: 28, textAlign: 'left', color: colors.theme.primary, marginBottom: 15 }}>
+                    {moment(Data).format('DD MMMM YYYY')}
+                </Text>
+            </View>
+        )
     }
 
     let [fontsLoaded] = useFonts({
@@ -89,12 +93,12 @@ const AddAppointment = ({ navigation }) => {
 
         return (
             <View style={{ backgroundColor: colors.theme.background, flexGrow: 1 }}>
-                <View style={{flexDirection: 'row'}}>
+                <View style={{ flexDirection: 'row' }}>
                     <BackButton onPress={() => { navigation.goBack() }} />
-                    <View style={{flex:1,justifyContent: "center",marginRight:'15%',alignItems: "center", paddingTop: '15%'}}>
-                    <Text style={{fontFamily: "SFProDisplayMedium", fontSize: 22, alignSelf:'center', color: colors.theme.title}}>Nuovo Appuntamento</Text>
+                    <View style={{ flex: 1, justifyContent: "center", marginRight: '15%', alignItems: "center", paddingTop: '15%' }}>
+                        <Text style={{ fontFamily: "SFProDisplayMedium", fontSize: 22, alignSelf: 'center', color: colors.theme.title }}>Nuovo Appuntamento</Text>
                     </View>
-                </View>  
+                </View>
                 {renderHeader(daySelected)}
                 <Calendar
                     key={key}
@@ -113,18 +117,106 @@ const AddAppointment = ({ navigation }) => {
                             selectedTextColor: colors.calendar_select.text,
                         }
                     }}
-                    renderHeader={(date) => <View style={{marginBottom: 55}}/>}
+                    renderHeader={(date) => <View style={{ marginBottom: 55 }} />}
                     theme={colors.calendar}
 
                 />
-                
+
                 <Divider width="100%" />
-                <ScrollView style={{height: "100%"}}>
-                    <View style={styles.container}>
-                        <View style={styles.checkboxContainer}>
-                            <Checkbox status={isSelected ? 'checked' : 'unchecked'} hideBox='false' tintColor='#aaaaaa' onPress={() => { setSelection(!isSelected);}} style/>
-                            <Text style={{margin:25, color: colors.theme.primary}}>Riserva l'intero negozio</Text>
+                <ScrollView style={{ height: "100%" }}>
+                    <View style={{flex: 1, width:'75%', alignSelf:'center', justifyContent:'center', alignItems:'center'}}>
+                        <View style={styles.section}>
+                            <Checkbox
+                                style={{margin: 0, borderRadius: 5 }}
+                                value={isChecked}
+                                onValueChange={setChecked}
+                                color={isChecked ? '#e78630' : undefined}
+                            />
+                            <Text style={{ margin: 25,fontSize: 16, fontFamily: 'SFProDisplayMedium', color: colors.theme.title }}>Riserva l'intero negozio</Text>
                         </View>
+                    </View>
+
+                    <View style={{ flexDirection: 'row', width: '100%' }}>
+                        <View style={{ width: '5%' }} />
+                        <View style={{ width: '45%' }} >
+                            <Text style={{ fontSize: 16, fontFamily: 'SFProDisplayMedium', color: colors.theme.title }}>Dalle:</Text>
+                        </View>
+                        <View style={{ width: '5%' }} />
+                        <View style={{ width: '45%' }} >
+                            <Text style={{ fontSize: 16, fontFamily: 'SFProDisplayMedium', color: colors.theme.title }}>Alle:</Text>
+                        </View>
+                        <View style={{ width: '5%' }} />
+                    </View>
+                    <View style={{ flexDirection: 'row', width: '100%' }}>
+                        <View style={{ width: '5%' }} />
+                        <Picker
+                            selectedValue={selectedFirstSlot}
+                            style={{ width: '40%' }}
+                            onValueChange={(itemValue, itemIndex) =>
+                                setSelectedFirstSlot(itemValue)
+                            }>
+                            <Picker.Item label="09:00" value="0" />
+                            <Picker.Item label="09:30" value="1" />
+                            <Picker.Item label="10:00" value="2" />
+                            <Picker.Item label="10:30" value="3" />
+                            <Picker.Item label="11:00" value="4" />
+                            <Picker.Item label="11:30" value="5" />
+                            <Picker.Item label="12:00" value="6" />
+                            <Picker.Item label="12:30" value="7" />
+                            <Picker.Item label="13:00" value="8" />
+                            <Picker.Item label="13:30" value="9" />
+                            <Picker.Item label="14:00" value="10" />
+                            <Picker.Item label="14:30" value="11" />
+                            <Picker.Item label="15:00" value="12" />
+                            <Picker.Item label="15:30" value="13" />
+                            <Picker.Item label="16:00" value="14" />
+                            <Picker.Item label="16:30" value="15" />
+                            <Picker.Item label="17:00" value="16" />
+                            <Picker.Item label="17:30" value="17" />
+                            <Picker.Item label="18:00" value="18" />
+                            <Picker.Item label="18:30" value="19" />
+                            <Picker.Item label="19:00" value="20" />
+                            <Picker.Item label="19:30" value="21" />
+                            <Picker.Item label="20:00" value="22" />
+                            <Picker.Item label="20:30" value="23" />
+                            <Picker.Item label="21:00" value="24" />
+                        </Picker>
+                        <View style={{ width: '10%' }} />
+                        <Picker
+                            selectedValue={selectedSecondSlot}
+                            style={{ width: '40%' }}
+                            onValueChange={(itemValue, itemIndex) =>
+                                setSelectedSecondSlot(itemValue)
+                            }>
+
+                            <Picker.Item label="09:00" value="0" />
+                            <Picker.Item label="09:30" value="1" />
+                            <Picker.Item label="10:00" value="2" />
+                            <Picker.Item label="10:30" value="3" />
+                            <Picker.Item label="11:00" value="4" />
+                            <Picker.Item label="11:30" value="5" />
+                            <Picker.Item label="12:00" value="6" />
+                            <Picker.Item label="12:30" value="7" />
+                            <Picker.Item label="13:00" value="8" />
+                            <Picker.Item label="13:30" value="9" />
+                            <Picker.Item label="14:00" value="10" />
+                            <Picker.Item label="14:30" value="11" />
+                            <Picker.Item label="15:00" value="12" />
+                            <Picker.Item label="15:30" value="13" />
+                            <Picker.Item label="16:00" value="14" />
+                            <Picker.Item label="16:30" value="15" />
+                            <Picker.Item label="17:00" value="16" />
+                            <Picker.Item label="17:30" value="17" />
+                            <Picker.Item label="18:00" value="18" />
+                            <Picker.Item label="18:30" value="19" />
+                            <Picker.Item label="19:00" value="20" />
+                            <Picker.Item label="19:30" value="21" />
+                            <Picker.Item label="20:00" value="22" />
+                            <Picker.Item label="20:30" value="23" />
+                            <Picker.Item label="21:00" value="24" />
+                        </Picker>
+                        <View style={{ width: '5%' }} />
+
                     </View>
                     <InputButton params={{ marginTop: 20, width: "75%" }} name="Conferma" icon="arrow-forward-outline" rotation="-45deg" />
                 </ScrollView>
@@ -137,20 +229,20 @@ const AddAppointment = ({ navigation }) => {
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: "center",
-      justifyContent: "center",
-    },
+
     checkboxContainer: {
-      flexDirection: "row",
-      marginBottom: 20,
-      
+        flexDirection: "row",
+        marginBottom: 20,
+
+    },
+    section: {
+        flexDirection: 'row',
+        alignItems: 'center',
     },
     checkbox: {
-      alignSelf: "center"
-    }
-  });
-  
+        margin: 8,
+    },
+});
+
 
 export default AddAppointment;
--- emad/src/screens/user/Login.js
+++ emad/src/screens/user/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Text,Dimensions } from "react-native";
+import { StyleSheet, View, Text, Dimensions } from "react-native";
+import Checkbox from 'expo-checkbox';
 import InputButton from "../../components/InputButton";
 import InputText from "../../components/InputText";
 import BackButton from "../../components/BackButton";
@@ -19,6 +20,7 @@ const Login = ({ navigation }) => {
 
   const [isModalVisible, setModalVisible] = useState(false);
   const [errorText, setErrorText] = useState('Default');
+  const [isChecked, setChecked] = useState(false);
 
   const toggleModal = () => {
     setModalVisible(!isModalVisible);
@@ -56,27 +58,27 @@ const Login = ({ navigation }) => {
     }
     navigation.navigate('TabBar');
   }
- 
+
   if (!fontsLoaded) {
     return <AppLoading />;
   } else {
     return (
       <View style={styles.screen}>
-        <StatusBar style='light'/>
-      
-          <Modal 
-            isVisible={isModalVisible}
-            statusBarTranslucent={true}
-            animationIn="jello"
-            animationOut="fadeOutDownBig"
-            hasBackdrop={true}
-            backdropOpacity={10}
-            backdropColor={"rgba(0, 0, 0, 0.3)"}
-            useNativeDriverForBackdrop={true}
-            hideModalContentWhileAnimating={true}
-            deviceHeight={height}
-            >
-              <View style={{padding: 20}}>
+        <StatusBar style='light' />
+
+        <Modal
+          isVisible={isModalVisible}
+          statusBarTranslucent={true}
+          animationIn="jello"
+          animationOut="fadeOutDownBig"
+          hasBackdrop={true}
+          backdropOpacity={10}
+          backdropColor={"rgba(0, 0, 0, 0.3)"}
+          useNativeDriverForBackdrop={true}
+          hideModalContentWhileAnimating={true}
+          deviceHeight={height}
+        >
+          <View style={{ padding: 20 }}>
             <View style={colors.topModal}>
               <Icon name="close-circle-outline" size={75} color={'#FFFFFF'} />
             </View>
@@ -85,8 +87,8 @@ const Login = ({ navigation }) => {
               <InputButton params={{ marginTop: '5%', width: "75%" }}
                 name="Conferma" icon="arrow-forward-outline" rotation="-45deg" onPress={toggleModal} />
             </View>
-            </View>
-          </Modal>
+          </View>
+        </Modal>
         <View style={styles.headerTop}>
           <BackButton onPress={() => { navigation.replace('SplashScreen') }} fixed />
           <Text style={{ fontSize: 30, fontFamily: 'SFProDisplayBold', width: "100%", color: 'white', alignSelf: "center", marginLeft: "20%", marginBottom: "5%" }}>
@@ -96,7 +98,15 @@ const Login = ({ navigation }) => {
         <View style={{ backgroundColor: colors.theme.background, height: "100%", alignItems: "center", paddingTop: 15 }}>
           <InputText params={{ marginTop: 25, width: "75%" }} name="Email" icon="mail-outline" rotation="0deg" value={userEmail} onChangeText={setUserEmail} secure='false' />
           <InputText params={{ marginTop: 10, width: "75%" }} name="Password" icon="key-outline" rotation="0deg" value={userPassword} onChangeText={setUserPassword} secure='true' />
-
+          <View style={styles.section}>
+            <Checkbox
+              style={{ margin: 0, borderRadius: 5 }}
+              value={isChecked}
+              onValueChange={setChecked}
+              color={isChecked ? '#e78630' : undefined}
+            />
+            <Text style={{ margin: 25, fontSize: 16, fontFamily: 'SFProDisplayMedium', color: colors.theme.title }}>Ricordami</Text>
+          </View>
           <InputButton params={{ marginTop: "5%", width: "75%", fontFamily: 'SFProDisplayMedium' }} name="ACCEDI" icon="arrow-forward-outline" rotation="-45deg"
             onPress={handleSubmitPress} />
         </View>
@@ -119,6 +129,15 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  section: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf:'flex-start',
+    paddingLeft: '12.5%'
+  },
+  checkbox: {
+    margin: 8,
   },
 });
 export default Login;
